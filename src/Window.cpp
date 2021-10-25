713e0b4adf5edc680b1cb5b3e923c3cfe2cad661
spookyGh0st <wr3r3w6j3boa6h@mailbox.org>
2021-10-25 15:20:14 +0200

    play around with windows

--- src/Window.cpp
+++ src/Window.cpp
@@ -8,6 +8,7 @@
 class Window {
 private:
     GLFWwindow *glfwWindow;
+    float xPos, yPos, size;
 
     static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     {
@@ -19,6 +20,9 @@ public:
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GLFW_MAJOR_VERSION);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GLFW_MINOR_VERSION);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+        glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
 
         glfwWindow = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
         if (glfwWindow == nullptr)
@@ -33,8 +37,19 @@ public:
             throw std::runtime_error("Failed to initialize GLAD");
         }
 
-        glViewport(0, 0, 800, 600);
         glfwSetFramebufferSizeCallback(glfwWindow, framebuffer_size_callback);
+        xPos = 800; yPos = 500; size = 800.0;
+        const GLFWvidmode *modes = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        xPos = float(modes->width)/2.0f;
+        yPos = float(modes->height)/2.0f;
+        move(0,0,0); // set Position and size
+    }
+
+    void move(float x, float y, float z){
+        xPos += x; yPos += y; size += z;
+        glfwSetWindowPos(glfwWindow, int(xPos-size/2),int(yPos-size/2));
+        glfwSetWindowSize(glfwWindow, int(size),int(size));
+        glViewport(0, 0, int(size), int(size));
     }
 
     int shouldClose(){
